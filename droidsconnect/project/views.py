3e99406e24eb244c71a53c479d4b32edf2a24561
johannilsson <johan.nilsson@voddler.com>
2010-07-29 20:11:35 +0200

    added edit view and other small changed...

--- droidsconnect/project/views.py
+++ droidsconnect/project/views.py
@@ -1,17 +1,18 @@
 from django import shortcuts
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.template import Context, loader, RequestContext
 from google.appengine.api import users
 from django.core.urlresolvers import reverse
-from droidsconnect.project.forms import ProjectForm
+from droidsconnect.project.forms import ProjectForm, ProjectModelForm
 from droidsconnect.project.models import Project
 import logging
 from google.appengine.ext import db
 import markdown
 
 def index(request):
-    q = db.GqlQuery("SELECT * FROM Project")
-    projects = q.fetch(20)
+    #q = db.GqlQuery("SELECT * FROM Project")
+    #projects = q.fetch(20)
+    projects = db.Query(Project).order('-created_at').fetch(100)
     template_values = {
         'project_list': projects,
     }
@@ -20,6 +21,7 @@ def index(request):
                            context_instance=RequestContext(request))
 
 def create(request):
+    # TODO: create a decorator: @requires_login(users.create_login_url...)
     user = users.get_current_user()
     if user is None:
         return HttpResponseRedirect(users.create_login_url(
@@ -32,7 +34,7 @@ def create(request):
             project = Project(title=form.clean_data['title'],
                           type=form.clean_data['type'],
                           description=form.clean_data['description'],
-                          owner=user)
+                          owner=request.user)
             project.needs_developer = form.clean_data['needs_developer']
             project.needs_artist = form.clean_data['needs_artist']
             project.needs_copywriter = form.clean_data['needs_copywriter']
@@ -63,21 +65,53 @@ def create(request):
     return shortcuts.render_to_response('project_create.html', template_values,
                            context_instance=RequestContext(request))
 
-def detail(request, project_id):
-    # TODO: add result to memcache...
+def edit(request, project_id):
+    user = users.get_current_user()
+    if user is None:
+        return HttpResponseRedirect(users.create_login_url('/'))
+    try:
+        key = db.Key(encoded=project_id)
+        project = db.Query(Project).filter('owner', request.user).filter('__key__', key).get()
+    except:
+        raise Http404
+    else:
+        if project is None:
+            raise Http404
 
-    key = db.Key(encoded=project_id)
-
-    q = db.GqlQuery("SELECT * FROM Project " +
-                "WHERE __key__ = :1", key)
-    project = q.get()
-
-    md = markdown.Markdown()
-    desc = md.convert(project.description)
+    form = ProjectModelForm(data=request.POST or None, instance=project)
+    if request.method == 'POST':
+        if form.is_valid():
+            try:
+                project = form.save(commit=False)
+            except ValueError, err:
+                pass
+            else:
+                project.put()
 
     template_values = {
+        'form': form,
         'project': project,
-        'description': desc,
+    }
+
+    return shortcuts.render_to_response('project_edit.html', template_values,
+                           context_instance=RequestContext(request))
+
+def detail(request, project_id):
+    # TODO: add result to memcache...
+    # event = Event.get_by_id(long(event_id)
+    try:
+        key = db.Key(encoded=project_id)
+        q = db.GqlQuery("SELECT * FROM Project " +
+                    "WHERE __key__ = :1", key)
+        project = q.get()
+    except:
+        raise Http404
+
+    md = markdown.Markdown()
+    description = md.convert(project.description)
+    template_values = {
+        'project': project,
+        'description': description,
     }
 
     return shortcuts.render_to_response('project_detail.html', template_values,
