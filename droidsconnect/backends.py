bb6ca59f2c04cf7e18f916505889481384e93543
johannilsson <johan.nilsson@voddler.com>
2010-07-29 20:07:39 +0200

    changed the account backend to also create an Account

--- droidsconnect/backends.py
+++ droidsconnect/backends.py
@@ -5,13 +5,19 @@ import logging
 
 class GoogleAccountBackend:
     def authenticate(self):
+        """
+        Called when a user is authenticated. If a Account does not exists
+        one will be created.
+        """
         logging.info("authenticate user")
         user = users.get_current_user()
         q = db.GqlQuery("SELECT * FROM Account where user = :1", user)
         account = q.get()
         if account is None:
             account = Account(user=user)
+            account.nickname = user.nickname()
+            if user.email():
+                account.email = user.email()
             account.put()
 
-        return user
-
+        return account
