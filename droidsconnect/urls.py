3d25a2be408ff868a1c7dda736c76d1002efcdf4
johannilsson <johan.nilsson@voddler.com>
2010-07-30 23:35:14 +0200

    added about section

--- droidsconnect/urls.py
+++ droidsconnect/urls.py
@@ -1,8 +1,5 @@
 from django.conf.urls.defaults import *
-
-# Uncomment the next two lines to enable the admin:
-#from django.contrib import admin
-#admin.autodiscover()
+from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
     (r'^$', 'droidsconnect.project.views.index'),
@@ -11,11 +8,5 @@ urlpatterns = patterns('',
     (r'^login/$', 'droidsconnect.auth.views.login'),
     (r'^logout/$', 'droidsconnect.auth.views.logout'),
     (r'^authenticate/$', 'droidsconnect.auth.views.authenticate'),
-    #(r'^project/$', 'droidsconnect.project.views.index'),
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    (r'^about/$', direct_to_template, {'template': 'about.html'}),
 )
