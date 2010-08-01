3379f57d15b79f1f5f313b07f56d308e8eb5ed9a
johannilsson <johan.nilsson@voddler.com>
2010-08-01 19:03:29 +0200

    removed boolean fields for needs_* and replaced them with text fields

--- droidsconnect/project/models.py
+++ droidsconnect/project/models.py
@@ -8,19 +8,28 @@ class Project(db.Model):
     title = db.StringProperty(required=True)
     description = db.TextProperty(required=True)
     type = db.StringProperty(required=True, choices=set(["App", "Game"]))
-    
-    needs_developer = db.BooleanProperty()
-    #needs_developer_list = db.TextProperty()
-    
-    needs_artist = db.BooleanProperty()
-    #needs_artist_list = db.TextProperty()
-    
-    needs_copywriter = db.BooleanProperty()
-    #needs_artist_list = db.TextProperty()
-    
+
+    developer_description = db.TextProperty()
+    artist_description = db.TextProperty()
+    copywriter_description = db.TextProperty()
+
     vcs_url = db.LinkProperty()
     project_url = db.LinkProperty()
     package_name = db.StringProperty()
 
     icon = db.BlobProperty()
 
+    def needs_developer(self):
+        if self.developer_description:
+            return True
+        return False
+
+    def needs_artist(self):
+        if self.artist_description:
+            return True
+        return False
+
+    def needs_copywriter(self):
+        if self.copywriter_description:
+            return True
+        return False
